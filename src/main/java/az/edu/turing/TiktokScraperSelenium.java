c01b081ebaef4881dde831b2e42862209a828ee1
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-06-23 12:28:33 +0400

    Some beatify

--- src/main/java/az/edu/turing/TiktokScraperSelenium.java
+++ src/main/java/az/edu/turing/TiktokScraperSelenium.java
@@ -18,49 +18,16 @@ public class TiktokScraperSelenium {
     private static final String TIKTOK_VIDEO_URL = "https://www.tiktok.com/@_mariam23/video/7379809969564159274?is_from_webapp=1&sender_device=pc";
     private static List<User> users = new ArrayList<>();
     private static List<Video> videos = new ArrayList<>();
-    private static List<String> commentProfileUrls;
+    //private static List<String> commentProfileUrls;
+    private static ExecutorService executor = Executors.newFixedThreadPool(10);
 
     public static void main(String[] args) {
         Configuration.setDriverPath();
         ChromeOptions options = Configuration.getChromeOptions();
-
-        ExecutorService executor = Executors.newFixedThreadPool(5);
         WebDriver driver = new ChromeDriver(options);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        User user1 = new User();
-        Video video1 = new Video();
 
-        try {
-            driver.get(TIKTOK_VIDEO_URL);
-
-            String username = Extractor.extractUsername(wait);
-            String videoId = Extractor.extractVideoId(wait);
-            int shareCount = Extractor.extractShareCount(wait);
-            int likeCount = Extractor.extractLikeCount(wait);
-            String uploadDate = Extractor.extractUploadDate(wait);
-            int commentCount = Extractor.extractCommentCount(wait);
-            int saveCount = Extractor.extractVideoSaveCount(wait);
-            commentProfileUrls = Extractor.generateTiktokProfileLinks(Extractor.extractUsernames(wait));
-            String profileUrl = Extractor.extractProfileLink(wait);
-            String videoUrl = Extractor.extractFirstVideoLinkFromProfile(driver, wait, profileUrl);
-
-            video1.setSoundPath(Downloader.downloadTikTokVideo("src/main/resources/", driver));
-
-            setVideoAndUserDetails(executor, driver, wait, user1, video1, profileUrl, commentCount, likeCount, saveCount, shareCount, uploadDate);
-
-            users.add(user1);
-            videos.add(video1);
-
-            printResults(username, videoId, shareCount, likeCount, uploadDate, commentCount, saveCount, profileUrl, user1, videoUrl);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (driver != null) {
-                driver.quit();
-            }
-            executor.shutdown();
-        }
+        runAll(TIKTOK_VIDEO_URL, wait, driver);
     }
 
     private static void setVideoAndUserDetails(ExecutorService executor, WebDriver driver, WebDriverWait wait, User user1, Video video1, String profileUrl, int commentCount, int likeCount, int saveCount, int shareCount, String uploadDate) throws InterruptedException, java.util.concurrent.ExecutionException {
@@ -105,7 +72,7 @@ public class TiktokScraperSelenium {
         }
     }
 
-    private static void printResults(String username, String videoId, int shareCount, int likeCount, String uploadDate, int commentCount, int saveCount, String profileUrl, User user1, String videoUrl) {
+    private static void printResults(String username, String videoId, int shareCount, int likeCount, String uploadDate, int commentCount, int saveCount, String profileUrl, User user1) {
         System.out.println("Publisher's username: " + username);
         System.out.println("Video ID: " + videoId);
         System.out.println("Share count: " + shareCount);
@@ -116,7 +83,42 @@ public class TiktokScraperSelenium {
         System.out.println("Profile URL: " + profileUrl);
         System.out.println("Follower Count: " + user1.getFollowerCount());
         System.out.println("Following Count: " + user1.getFollowingCount());
-        System.out.println("Video URL: " + videoUrl);
-        commentProfileUrls.forEach(System.out::println);
+        //commentProfileUrls.forEach(System.out::println);
+    }
+
+    private static void runAll (String videoURL, WebDriverWait wait, WebDriver driver){
+        try {
+            driver.get(videoURL);
+            User user1 = new User();
+            Video video1 = new Video();
+
+            String username = Extractor.extractUsername(wait);
+            String videoId = Extractor.extractVideoId(wait);
+            int shareCount = Extractor.extractShareCount(wait);
+            int likeCount = Extractor.extractLikeCount(wait);
+            String uploadDate = Extractor.extractUploadDate(wait);
+            int commentCount = Extractor.extractCommentCount(wait);
+            int saveCount = Extractor.extractVideoSaveCount(wait);
+           // commentProfileUrls = Extractor.generateTiktokProfileLinks(Extractor.extractUsernames(wait));
+            String profileUrl = Extractor.extractProfileLink(wait);
+            String videoUrl = Extractor.extractFirstVideoLinkFromProfile(driver, wait, profileUrl);
+
+            video1.setSoundPath(Downloader.downloadTikTokVideo("src/main/resources/", driver));
+
+            setVideoAndUserDetails(executor, driver, wait, user1, video1, profileUrl, commentCount, likeCount, saveCount, shareCount, uploadDate);
+
+            users.add(user1);
+            videos.add(video1);
+
+            printResults(username, videoId, shareCount, likeCount, uploadDate, commentCount, saveCount, profileUrl, user1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (driver != null) {
+                driver.quit();
+            }
+            executor.shutdown();
+        }
     }
 }
