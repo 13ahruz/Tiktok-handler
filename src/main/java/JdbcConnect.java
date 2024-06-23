07399cff342f13137655627c41ce19b00dab3b0b
aliagasiyev <agasiyevali2@gmail.com>
2024-06-23 16:58:53 +0400

    DbConnection attached

---
+++ src/main/java/JdbcConnect.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcConnect {
+    final String url = "jdbc:postgresql://localhost:5432/postgres";
+    final String user = "postgres";
+    final String password = "postgres";
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
